cd6bb88f8a2a43496cb14d4deaea0a6ffd3d069a
Bronislav <CJLaB4er@gmail.com>
2022-12-03 00:17:36 +0300

    выполнены задачи 1 и 2

--- Seminar_1/task_2/Program.cs
+++ Seminar_1/task_2/Program.cs
@@ -6,18 +6,25 @@
 // 22 3 9 -> 22
 
 Console.Clear(); // очистка консоли
+
 Console.WriteLine("Введите первое число...");
+
 int max_numb = int.Parse(Console.ReadLine()!); // вводим первое число и назначаем его максимальным
+
 Console.WriteLine("Введите второе число...");
+
 int temp_numb = int.Parse(Console.ReadLine()!); // вводим второе число
-if(temp_numb > max_numb) // проверяем, 
+
+if (temp_numb > max_numb) // сравниваем temp_numb и max_numb
 {
-    max_numb = temp_numb;
+    max_numb = temp_numb; // если условие верно, то присваиваем max_numb значение temp_numb
 }
 Console.WriteLine("Введите третье число...");
+
 temp_numb = int.Parse(Console.ReadLine()!);
-if(temp_numb > max_numb)
+
+if (temp_numb > max_numb)
 {
     max_numb = temp_numb;
 }
-Console.WriteLine(max_numb);
+Console.WriteLine(max_numb); // вывод в консоль значение переменной max_numb
