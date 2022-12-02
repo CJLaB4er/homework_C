78d2f3313127e444c8550cff1b9144fafe84b0aa
Bronislav <CJLaB4er@gmail.com>
2022-12-03 00:08:59 +0300

    1

--- task_2/Program.cs
+++ task_2/Program.cs
@@ -4,3 +4,20 @@
 // 2, 3, 7 -> 7
 // 44 5 78 -> 78
 // 22 3 9 -> 22
+
+Console.Clear(); // очистка консоли
+Console.WriteLine("Введите первое число...");
+int max_numb = int.Parse(Console.ReadLine()!); // вводим первое число и назначаем его максимальным
+Console.WriteLine("Введите второе число...");
+int temp_numb = int.Parse(Console.ReadLine()!); // вводим второе число
+if(temp_numb > max_numb) // проверяем, 
+{
+    max_numb = temp_numb;
+}
+Console.WriteLine("Введите третье число...");
+temp_numb = int.Parse(Console.ReadLine()!);
+if(temp_numb > max_numb)
+{
+    max_numb = temp_numb;
+}
+Console.WriteLine(max_numb);
