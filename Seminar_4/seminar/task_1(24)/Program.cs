44645cf4d72b6a032c329c36f02410ea6005fa45
Bronislav <CJLaB4er@gmail.com>
2022-12-10 19:38:09 +0300

    выполнены задания четвёртого семинара

---
+++ Seminar_4/seminar/task_1(24)/Program.cs
@@ -0,0 +1,22 @@
+﻿// Задача 24: Напишите программу, которая принимает на вход число (А) и выдаёт сумму чисел от 1 до А.
+// 7 -> 28
+// 4 -> 10
+// 8 -> 36
+
+Console.Clear();
+
+Console.WriteLine("Введите число...");
+
+int num = int.Parse(Console.ReadLine()!);
+
+int GetSum(int numb)
+{
+    int sum = 0;
+    for (int i = 1; i <= numb; i++)
+    {
+        sum += i;
+    }
+    return sum;
+}
+
+Console.WriteLine(GetSum(num));
---
+++ Seminar_4/seminar/task_2(26)/Program.cs
@@ -0,0 +1,15 @@
+﻿// Задача 26: Напишите программу, которая принимает на вход число и выдаёт количество цифр в числе.
+// 456 -> 3
+// 78 -> 2
+// 89126 -> 5
+
+Console.Clear();
+
+Console.WriteLine("Введите число...");
+
+int numb = int.Parse(Console.ReadLine()!);
+
+int Numbers(int number)
+{
+    
+}
