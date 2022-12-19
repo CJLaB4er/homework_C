56316f2a557da4fd90a8b5cd638bd97251738efa
Bronislav <CJLaB4er@gmail.com>
2022-12-19 20:41:41 +0300

    выполнено задание 36

--- Seminar_5/homework/task_2(36)/Program.cs
+++ Seminar_5/homework/task_2(36)/Program.cs
@@ -1,5 +1,40 @@
-﻿Задача 36: Задайте одномерный массив, заполненный случайными числами. Найдите сумму элементов, стоящих на нечётных позициях.
+﻿// Задача 36: Задайте одномерный массив,
+// заполненный случайными числами.
+// Найдите сумму элементов,
+// стоящих на нечётных позициях.
 
-[3, 7, 23, 12] -> 19
+// // [3, 7, 23, 12] -> 19
 
-[-4, -6, 89, 6] -> 0
+// [-4, -6, 89, 6] -> 0
+
+Console.Clear();
+
+Console.WriteLine("Введите количество элементов массива...");
+
+int num = int.Parse(Console.ReadLine()!);
+
+int[] GetArray(int size)
+{
+    int[] newArray = new int[size];
+    Console.Write("В массиве [");
+    for (int i = 0; i < size; i++)
+    {
+        newArray[i] = new Random().Next(-99, 100);
+        if (i != size - 1) Console.Write($"{newArray[i]}, ");
+        else Console.Write($"{newArray[i]}");
+    }
+    Console.Write("]");
+    return newArray;
+}
+
+void CheckArray(int[] arr)
+{
+    int count = 0;
+    for (int i = 1; i < arr.Length; i += 2)
+    {
+        count += arr[i];
+    }
+    Console.Write($", сумма элементов на нечётных индексах равна {count}.");
+}
+
+CheckArray(GetArray(num));
