d5f5609142921c6a4b1fdf0e87a5a40415422204
Bronislav <CJLaB4er@gmail.com>
2022-12-19 19:49:13 +0300

    Сделана задача 34

---
+++ Seminar_5/homework/task_1(34)/Program.cs
@@ -0,0 +1,35 @@
+﻿// Задача 34: Задайте массив заполненный
+// случайными положительными трёхзначными
+// числами. Напишите программу,
+// которая покажет количество чётных чисел в массиве.
+
+// [345, 897, 568, 234] -> 2
+
+Console.Clear();
+
+int[] GetArray()
+{
+    int[] newArray = new int[3];
+    Console.Write($"Массив [");
+    for (int i = 0; i < newArray.Length; i++)
+    {
+        newArray[i] = new Random().Next(1, 100);
+        if (i != newArray.Length-1) Console.Write($"{newArray[i]}, ");
+        else Console.Write($"{newArray[i]}");
+    }
+    Console.Write($"]");
+    return newArray;
+}
+
+void checkArray(int[] arr)
+{
+    int count = 0;
+    
+    foreach (int el in arr)
+    {
+        if (el % 2 == 0) count++;
+    }
+    Console.Write($" имеет количество чётных чисел равное {count}");
+}
+
+checkArray(GetArray());
---
+++ Seminar_5/seminar/task_2(32)/Program.cs
@@ -0,0 +1,43 @@
+﻿// Задача 32: Напишите программу замена элементов массива:
+// положительные элементы замените на
+// соответствующие отрицательные, и наоборот.
+// [-4, -8, 8, 2] -> [4, 8, -8, -2] 
+
+Console.Clear();
+Console.WriteLine("Введите количество элементов массива...");
+
+int[] array = new int[int.Parse(Console.ReadLine()!)];
+
+int[] FillArray(int[] arr)
+{
+    Console.Write($"Исходный массив выглядит так: ");
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = new Random().Next(-9, 10);
+        Console.Write($"{arr[i]} ");
+    }
+    Console.WriteLine();
+    return arr;
+}
+
+int[] ReverseArray(int[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr[i] *= -1;
+    }
+    return arr;
+}
+
+void PrintArray(int[] arr)
+{
+    Console.Write($"После смены знаков массив выглядит так: ");
+
+    for (int i = 0; i< arr.Length; i++)
+    {
+        Console.Write($"{arr[i]} ");
+    }
+}
+array = FillArray(array);
+array = ReverseArray(array);
+PrintArray(array);
