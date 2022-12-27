fed10bba7dca5700afb01c64680e6f80704afa29
Bronislav <CJLaB4er@gmail.com>
2022-12-27 18:48:54 +0300

    Исправлено решение задачи №47

--- Seminar_7/homework/task_47/Program.cs
+++ Seminar_7/homework/task_47/Program.cs
@@ -1,2 +1,50 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 47. Задайте двумерный массив размером m×n,
+// заполненный случайными вещественными числами.
+
+// m = 3, n = 4.
+
+// 0,5 7 -2 -0,2
+
+// 1 -3,3 8 -9,9
+
+// 8 7,8 -7,1 9
+
+Console.Clear();
+
+double[,] CreateArrayMN()
+{
+    Console.WriteLine("Создаем массив размерностью m x n...");
+    Console.WriteLine("Введите число строк массива...");
+    int m = int.Parse(Console.ReadLine()!);
+    Console.WriteLine("Введите число столбцов массива...");
+    int n = int.Parse(Console.ReadLine()!);
+    double[,] array = new double[m, n];
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            // В строчке ниже весь смысл задания - 
+            // метод NextDouble возвращает псевдослучайное число от 0,0 до 1,0
+            // поэтому домножаем его на псевдослучайное число от -10 до 10
+            // и округляем результат до двух знаков после запятой.
+            array[i, j] = Math.Round(new Random().NextDouble() * new Random().Next(-10,11), 2);
+        }
+    }
+    return array;
+}
+
+void PrintArray(double[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            if (array[i, j] > 0) Console.Write($" {array[i, j]} ");
+            else Console.Write($"{array[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+PrintArray(CreateArrayMN());
+
+
