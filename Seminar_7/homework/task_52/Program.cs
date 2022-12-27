90eed360d7c71d8d5cd5be30edb16f13fb6823ed
Bronislav <CJLaB4er@gmail.com>
2022-12-27 19:38:05 +0300

    решена задача №52

---
+++ Seminar_7/homework/task_52/Program.cs
@@ -0,0 +1,61 @@
+﻿// Задача 52. Задайте двумерный массив из целых чисел. 
+// Найдите среднее арифметическое элементов в каждом столбце.
+
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// Среднее арифметическое каждого столбца: 4,6; 5,6; 3,6; 3.
+
+Console.Clear();
+
+int[,] CreateArrayMN()
+{
+    Console.WriteLine("Создаем массив размерностью m x n...");
+    Console.WriteLine("Введите число строк массива...");
+    int m = int.Parse(Console.ReadLine()!);
+    Console.WriteLine("Введите число столбцов массива...");
+    int n = int.Parse(Console.ReadLine()!);
+    int[,] array = new int[m, n];
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            array[i, j] = new Random().Next(1, 10);
+        }
+    }
+    return array;
+}
+
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            if (array[i, j] >= 0) Console.Write($" {array[i, j]} ");
+            else Console.Write($"{array[i, j]} ");
+        }
+        Console.WriteLine("");
+    }
+}
+
+void AvgColoumnArray(int[,] array)
+{
+    double count = 0;
+    double result = 0;
+    for (int i = 0; i < array.GetLength(1); i++)
+    {
+        for (int j = 0; j < array.GetLength(0); j++)
+        {
+            count += array[j, i];
+        }
+        result = count / array.GetLength(0);
+        Console.WriteLine($"Среднеарифметическое {i+1}го столбца - {result} ");
+        count = 0;
+    }
+}
+
+int[,] newArray = CreateArrayMN();
+PrintArray(newArray);
+AvgColoumnArray(newArray);
