e25951a0c9bc115708b0cd5b07c7a279e42665b6
Бронислав Scomi <cjlab4er@gmail.com>
2023-01-01 17:19:09 +0500

    решена задача №64

---
+++ seminar_9/homework/task_64/Program.cs
@@ -0,0 +1,67 @@
+﻿// Задача 64: Задайте значение N. Напишите программу,
+// которая выведет все натуральные числа в промежутке от N до 1.
+// Выполнить с помощью рекурсии.
+// Доп условие семинара - выводить только четные числа
+
+// N = 5 -> "5, 4, 3, 2, 1"
+// N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1"
+
+Console.Clear();
+
+Console.WriteLine("Введите значение N...");
+
+int n = int.Parse(Console.ReadLine()!);
+
+Console.Clear();
+
+Console.WriteLine($"Вывод всех чисел от N до 1...");
+
+Console.Write($"N = {n} -> ");
+
+void WriteNumb(int n)
+{
+    if (n == 1)
+    {
+        Console.Write("1");
+    }
+    else
+    {
+        Console.Write($"{n} ");
+        WriteNumb(n - 1);
+    }
+
+}
+
+Console.Write($"N = {n} -> ");
+
+void WriteEvenNumb(int n)
+{
+    if (n == 2)
+    {
+        Console.Write("2");
+    }
+    else
+    {
+        if (n % 2 != 0)
+        {
+            WriteEvenNumb(n - 1);
+        }
+        else
+        {
+            Console.Write($"{n} ");
+            WriteEvenNumb(n - 2);
+        }
+
+    }
+
+}
+
+WriteNumb(n);
+
+Console.WriteLine();
+
+Console.WriteLine($"Вывод всех чётных чисел от N до 2...");
+
+Console.Write($"N = {n} -> ");
+
+WriteEvenNumb(n);
